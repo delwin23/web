b14359ee78e6a266aad886ed7b7b2b521164ffe1
Delwin Kuriakose <128991861+delwin23@users.noreply.github.com>
2023-12-06 11:17:04 +0530

    Add files via upload

---
+++ sessionalexam.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+<center>
+	<title>Sessional Exam Time Table</title>
+</center>
+<style>
+body{
+	font-family: 'Arial', sans-serif;
+      background-color: #f8f9fa;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+}
+table{
+	 border-collapse: collapse;
+      width: 100%;
+      margin: 20px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+      border-radius: 10px;
+      background: linear-gradient(45deg, #92c3d2, #ecb6e8);
+     }
+th, td {
+      padding: 15px;
+      text-align: left;
+      border-bottom: 1px solid #e0e0e0;
+      color: #333;
+    }
+
+th {
+      background-color: #007bff;
+      color: #fff;
+      font-weight: bold;
+    }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+	</style>
+	</head>
+  <body>
+  	<center>
+  	<h1><u><b>Sessional exam time table</b></u><h1>
+  	<br><br><table cellpadding="8" border="4">
+  <tr>
+  	<th>date</th>
+  	<th>subject</th>
+  	<th>time</th>
+  </tr>
+     
+     <tr>
+     	<td>20-10-2020</td>
+     	<td>geography</td>
+     	<td>9:30 am-12:30pm</td>
+     </tr>
+     
+     	<tr>
+     		<td>22-10-2020</td>
+     		<td>maths</td>
+     		<td>9:30 am-12:30 pm</td>
+     	</td>
+     	
+     	<tr>
+     		<td>24-10-2020</td>
+     		<td>Data structures</td>
+                <td>9:30 am-12:30 pm</td>
+        </tr>
+        
+        <tr>
+        	<td>26-10-2020</td>
+        	<td>digital logic</td>
+        	<td>1:30 pm-4:30 pm</td>
+        </tr>
+        </table>
+        </body>
+        </html>  		
+  
